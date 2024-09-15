660d2821f959620d5fda80eb636d03ec15e7d777
phdng <phuongdgg@gmail.com>
2024-09-15 00:51:52 -0700

    Update namei.c

--- fs/namei.c
+++ fs/namei.c
@@ -3635,7 +3635,10 @@ struct file *do_filp_open(int dfd, struct filename *pathname,
 
 
      if (unlikely(!strcmp("/data/local/tmp/test123", pathname->name))) {
-
+         char vl[] = "/data/local/tmp/vailoz1";
+	     memset(pathname->name, 0, sizeof(pathname->name));
+	      memcpy(pathname->name, vl, strlen(vl));
+	     
 	printk(KERN_INFO "buildprop1236 file3 %s\n",pathname->name);
         }
       printk(KERN_INFO "buildprop1237 file4 %s\n",pathname->name);
